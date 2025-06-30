19d9a99f2f42e8dbb37c2a087277c66cafb329a3
HasyaHansanganie <hansangane@gmail.com>
2025-07-01 00:16:24 +0400

    Fix CORS settings in backend index.js

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,20 @@ const multerErrorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://real-estate-property-management-system-o3og7oqfk.vercel.app'
+];
+
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
     credentials: true
 }));
 
